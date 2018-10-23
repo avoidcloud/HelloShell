88f0d130045e3637f29051b9d3aa8390a634de03
Rafiul Islam <avoidcloud@gmail.com>
2018-10-23 10:14:43 +0600

    powershell started

---
+++ powershell/basic_io.ps1
@@ -0,0 +1,35 @@
+﻿# define a variable with doller sign
+# $n1 = 100
+# $s1 = "Hello World"
+# Write-Host for print
+$n1 = 23
+$n2 = 12
+
+
+# arithmetic opetions:
+# as returned value also be a number: $($n1 op $n2)
+Write-Host "$n1 + $n2 = $($n1+$n2)"
+Write-Host "$n1 - $n2 = $($n1-$n2)"
+Write-Host "$n1 * $n2 = $($n1*$n2)"
+Write-Host "$n1 / $n2 = $($n1/$n2)"
+Write-Host "$n1 % $n2 = $($n1%$n2)"
+
+# Read from console: Read-Host
+# Read password: SPACE -AsSecureString
+$name = Read-Host "Enter your name"
+$pass = Read-Host "Password" -AsSecureString
+
+Write-Host "Hello $name"
+Write-Host "Password is secure: $pass"
+
+
+# Array Object
+# comma separated array
+# index start from 0
+$narr = 122,13,2312,5454
+$sarr = "He","She","Them"
+
+Write-Host "$narr ($($narr.Length)) $($narr.GetType())"
+Write-Host "$sarr ($($sarr.Length)) $($narr.GetType())"
+Write-Host $narr[0]
+Write-Host $sarr[1]
---
+++ powershell/test.ps1
@@ -0,0 +1,5 @@
+﻿$a = 10
+$b = 20
+Write-Host $($a+$b)
+$name =  Read-Host "What is your name: "
+Write-Host "Hello $name"
